9445964bb3cae4dbd7dfcc69038e2a1dbc5bddbe
Azrio <38596941+Zechst@users.noreply.github.com>
2023-03-19 20:43:09 +0800

    style: increase gap between icons

--- components/logo-cloud.js
+++ components/logo-cloud.js
@@ -9,7 +9,7 @@ export default function LogoCloud() {
   return (
     <div className="py-20 sm:py-0 px-10 sm:px-20 lg:px-32">
       <h2 className="font-bold text-white text-3xl">Featured Clients</h2>
-      <div className="flex gap-x-6 sm:gap-x-4 mt-5">
+      <div className="flex gap-x-8 sm:gap-x-6 mt-5">
         <Image width={110} height={30} src={natGeoLogo} />
         <Image width={110} height={30} src={skyTravelLogo} />
         <Image width={100} height={30} src={airbnbLogo} />
